58a1addb8ae2c8bcc7b9bafeaf528087f78900cd
Felipe Dioguardi <felipe.dioguardi@colescba.org.ar>
2024-11-19 14:30:46 -0300

    init: practica 3

---
+++ practica_3/1_counter.py
@@ -0,0 +1,119 @@
+from web3 import Web3, exceptions
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+NODE: str = os.getenv("NODE")
+w3: Web3 = Web3(Web3.HTTPProvider(NODE))
+
+if not w3.isConnected():
+    exit("Unable to connect to the network")
+
+ABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": False,
+		"inputs": [
+			{
+				"indexed": True,
+				"internalType": "address",
+				"name": "by",
+				"type": "address"
+			},
+			{
+				"indexed": False,
+				"internalType": "string",
+				"name": "operation",
+				"type": "string"
+			},
+			{
+				"indexed": False,
+				"internalType": "uint8",
+				"name": "newValue",
+				"type": "uint8"
+			}
+		],
+		"name": "CounterModified",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "addToWhitelist",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decrement",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "increment",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "removeFromWhitelist",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "whitelist",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+contract = w3.eth.contract(address="0x03ca1af34709b73f07a3233e08b7dbdae5d8afb0", abi=ABI)
+
